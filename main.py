c92c81cefecc418ff5eea329b25d9faca85e6339
mrdodgerx <dodgerxc47@gmail.com>
2025-03-08 02:34:37 +0800

    remove unwated code

--- main.py
+++ main.py
@@ -3,22 +3,19 @@ from modules.githubproxy import ProxyManager
 
 if __name__ == '__main__':
     
-    # scraper = SSLProxies(max_workers=10, timeout=5)
-
-    # # Fetch proxies and request www.google.com using all of them
-    # url = "https://www.google.com"
-    # response = scraper.request.get(url)
-    
+    # Create instances of ProxyManager
     proxy_manager = ProxyManager(protocol="socks5")
 
     # Example: Make a GET request using shortcut
     response1 = proxy_manager.request.get("https://httpbin.org/ip")
     if response1:
         print("GET Status Code:", response1.status_code)
-        print("GET Response JSON:sadasd", response1.text)
+        print("GET Response JSON:", response1.json())
        
+    ssl_proxies = SSLProxies()
+    response = ssl_proxies.request.get("https://httpbin.org/ip")
 
-    # # Print successful responses
-    # if response:
-    #     print("Status Code:", response.status_code)
-    #     print("Response JSON:", response.text)
+    # Print successful responses
+    if response:
+        print("Status Code:", response.status_code)
+        print("Response JSON:", response.json())
